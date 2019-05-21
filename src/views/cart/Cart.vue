41a59d577380075b647eebbb0ab67f03f01a51fd
cy <chen479265804@163.com>
2019-05-21 16:42:41 +0800

    小米c商城第一次提交

---
+++ src/views/cart/Cart.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <van-nav-bar title="购物车" left-text="返回" @click-left="$router.go(-1)" left-arrow>
+            <van-icon name="search" slot="right">
+        </van-nav-bar>
+        <h1>购物车</h1>
+        <p v-for="(item,index) in goods" :key="index">{{item.name}}--{{item.num}}</p>
+    </div>
+</template>
+<script>
+// 导入mapstate方法
+import {mapState} from 'vuex';
+import {NavBar,Icon}from 'vant';
+export default {
+    computed:{
+        ...mapState(["goods"])
+    },
+    comments:{
+        [NavBar.name]:NavBar,
+        [Icon.name]:Icon
+    }
+}
+</script>
+<style scoped>
+
+</style>
